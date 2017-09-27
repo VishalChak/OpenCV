ae137bf8c62747723949b1d9fc731b10b581bb40
VishalChak <vishalbabu.in@gmail.com>
2017-09-27 22:01:21 +0530

    fistr pr

---
+++ vedios.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 23:42:10 2017
+
+@author: vishal
+"""
+
+import numpy as np
+import cv2
+
+
+path = ""
+cap = cv2.VideoCapture('vtest.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
